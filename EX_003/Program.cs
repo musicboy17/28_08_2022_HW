c8ef60be43f47e403a1803bebc607d0ad09203ca
musicboy17 <pratham@yandex.ru>
2022-08-28 23:11:14 +0300

    EX_003_First_step

--- EX_003/Program.cs
+++ EX_003/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+Console.WriteLine("Введите размер двумерного масситва");
+Console.WriteLine("количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int rowsLength = rows;
+int columnsLength = columns;
+
+int [,] table = new int[rows, columns]; 
+
+void FillArray(int [,] table)
+{
+    for (int i = 0; i < rowsLength; i++)
+    {
+        for (int j = 0; j < columnsLength; j++)
+        {
+            table[i,j] = new Random().Next(-10, 10);
+        }
+    }
+}
+FillArray(table);
+
+for (rows = 0; rows < rowsLength; rows++)
+{
+    for (columns = 0; columns  < columnsLength; columns++)
+    {
+        Console.Write($"{table[rows, columns]} ");
+    }
+    Console.WriteLine();
+}
+
+
